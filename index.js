667390c3331c1d768e3470b0437b73a539bd4829
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-04-26 20:23:49 +0500

    all commit

--- index.js
+++ index.js
@@ -70,11 +70,11 @@
 
 //====================================================
 
-var reverse = function (x) {
-  const reversed =
-    parseInt(Math.abs(x).toString().split("").reverse().join("")) *
-    Math.sign(x);
-  return reversed < Math.pow(-2, 31) || reversed > Math.pow(2, 31) - 1
-    ? 0
-    : reversed;
-};
+// var reverse = function (x) {
+//   const reversed =
+//     parseInt(Math.abs(x).toString().split("").reverse().join("")) *
+//     Math.sign(x);
+//   return reversed < Math.pow(-2, 31) || reversed > Math.pow(2, 31) - 1
+//     ? 0
+//     : reversed;
+// };
