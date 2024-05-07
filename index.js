034d1db1958ad08b198985d274d29bb0a5a2853c
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-05-07 14:01:34 +0500

    all problems commented

--- index.js
+++ index.js
@@ -150,21 +150,23 @@
 // console.log(intToRoman(58)); // Output: "LVIII"
 // console.log(intToRoman(1994)); // Output: "MCMXCIV"
 
-function climbStairs(n) {
-  if (n === 1) return 1;
+// ===================================================================================
 
-  let dp = new Array(n + 1);
-  dp[1] = 1;
-  dp[2] = 2;
+// function climbStairs(n) {
+//   if (n === 1) return 1;
 
-  for (let i = 3; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
-  }
+//   let dp = new Array(n + 1);
+//   dp[1] = 1;
+//   dp[2] = 2;
 
-  return dp[n];
-}
+//   for (let i = 3; i <= n; i++) {
+//     dp[i] = dp[i - 1] + dp[i - 2];
+//   }
 
-// Test cases
-console.log(climbStairs(2)); // Output: 2
-console.log(climbStairs(3)); // Output: 3
-console.log(climbStairs(8)); // Output: 34
+//   return dp[n];
+// }
+
+// // Test cases
+// console.log(climbStairs(2)); // Output: 2
+// console.log(climbStairs(3)); // Output: 3
+// console.log(climbStairs(8)); // Output: 34
